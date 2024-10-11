739eb5c0aeb888262d6f30d89b3928d85177d81d
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2024-10-11 15:49:34 +0700

    completed brand filter

---
+++ src/components/BrandFilter.jsx
@@ -0,0 +1,48 @@
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+const BrandFilter = ({ isLoading, selectedBrands, onSelectBrands }) => {
+  const brands = [
+    { id: 'brand-1', name: 'SAMSUNG', count: 578 },
+    { id: 'brand-2', name: 'LG', count: 125 },
+    { id: 'brand-3', name: 'SONY', count: 755 },
+    { id: 'brand-4', name: 'SAMSUNG', count: 578 },
+    { id: 'brand-5', name: 'LG', count: 125 },
+    { id: 'brand-6', name: 'SONY', count: 755 },
+  ];
+
+  const handleCheckboxChange = (id) => {
+    onSelectBrands((prev) =>
+      prev.includes(id) ? prev.filter((p) => p !== id) : [...prev, id]
+    );
+  };
+
+  return (
+    <div className="aside">
+      <h3 className="aside-title">Brand</h3>
+      <div className="checkbox-filter">
+        {brands.map((brand) =>
+          isLoading ? (
+            <Skeleton height={20} />
+          ) : (
+            <div className="input-checkbox" key={brand.id}>
+              <input
+                type="checkbox"
+                id={brand.id}
+                checked={selectedBrands.includes(brand.id)}
+                onChange={() => handleCheckboxChange(brand.id)}
+              />
+              <label htmlFor={brand.id}>
+                <span></span>
+                {brand.name}
+                <small>({brand.count})</small>
+              </label>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default BrandFilter;

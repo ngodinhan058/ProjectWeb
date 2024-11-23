c03847ab7b79635eaa422d1abe83749d453437aa
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2024-11-23 09:46:50 +0700

    add message

--- src/components/api/config.jsx
+++ src/components/api/config.jsx
@@ -1,3 +1,2 @@
-
 export const BASE_URL =
-  'https://721f-2405-4802-80b1-9530-70ee-e225-8a96-739d.ngrok-free.app/api/v1/';
+  'https://ee5b-2405-4802-80dd-1ca0-35f9-a8d7-afcb-14af.ngrok-free.app/api/v1/';

301cf1223114fae373d7e81dbf7f51c190cd3cf0
ngodinhan058 <dinhanngo058@gmail.com>
2024-12-10 07:30:36 +0700

    update banner

--- src/components/api/config.jsx
+++ src/components/api/config.jsx
@@ -1,3 +1,3 @@
 export const BASE_URL =
-'https://7d6c-2405-4802-9154-3a80-98cc-3d7a-faa8-57e7.ngrok-free.app/api/v1/';
+'http://192.168.1.6:8080/api/v1/';
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,119 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect, useRef } from 'react';
+import { Animated, useWindowDimensions, } from 'react';
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { useNavigate } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import DesktopCollections from "../components/DesktopCollections";
 import MobileCollections from "../components/MobileCollections";
+import Banner from "../components/Banner";
 import { BASE_URL } from "../components/api/config";
 import { axiosInstance } from "../components/api/axiosConfig";
-// const mockCollections = [
-//   {
-//     id: 1,
-//     name: "Collections Top 1",
-//     image:
-//       "https://pos.nvncdn.com/be3294-43017/campaign/20240524_oNYjSZ5b.jpeg",
-//     products: [
-//       {
-//         id: "00f81ac1-1e15-4e26-9aac-a44c2873f4e8",
-//         name: "iPhone 16",
-//         image:
-//           "https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/42/329149/iphone-16-pro-max-titan-den-1-638638962017739954-750x500.jpg",
-//         price: "234đ",
-//       },
-//       {
-//         id: "31360000-0000-0000-0000-000000000000",
-//         name: "Xiaomi 14 Ultra",
-//         image:
-//           "https://cdn.tgdd.vn/Products/Images/42/313889/xiaomi-14-ultra-1-750x500.jpg",
-//         price: "180.000 ₫",
-//       },
-//       {
-//         id: "50038924-ade1-48bc-8336-73b11255bfb5",
-//         name: "MacBook Air M1",
-//         image:
-//           "https://cdn.tgdd.vn/Products/Images/44/231244/grey-1-750x500.jpg",
-//         price: "190.000 ₫",
-//       },
-//       {
-//         id: "31380000-0000-0000-0000-000000000000",
-//         name: "Xiaomi 14T Pro",
-//         image:
-//           "https://cdn.tgdd.vn/Products/Images/42/313889/xiaomi-14-ultra-1-750x500.jpg",
-//         price: "1.090.000 ₫",
-//       },
-//       {
-//         id: "32e0ed13-66fd-4608-b783-b999cecfbd40",
-//         name: "Laptop Gaming MSI GF63",
-//         image:
-//           "https://cdn.tgdd.vn/Products/Images/44/231244/grey-1-750x500.jpg",
-//         price: "1.430.000 đ",
-//       },
-//       {
-//         id: "a547c7cf-f93e-4833-99fc-b82ffd1c0242",
-//         name: "iPhone 16 Pro Max",
-//         image:
-//           "https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/42/329149/iphone-16-pro-max-titan-den-1-638638962017739954-750x500.jpg",
-//         price: "1.490.00 đ",
-//       },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     name: "Collection Trending",
-//     image:
-//       "https://pos.nvncdn.com/be3294-43017/campaign/20241123_WLCk6DeD.jpeg",
-//     products: [
-//       {
-//         id: "dd65a34e-f646-462e-a3ce-8d78d28460fe",
-//         name: "Samsung Galaxy S24 Ultra",
-//         image:
-//           "https://cdn.tgdd.vn/Products/Images/42/326348/samsung-galaxy-z-fold6-xanh-navy-1-750x500.jpg",
-//         price: "1.499.000 ₫",
-//       },
-//       {
-//         id: "d7b72420-5642-48f7-895e-6204b572dc51",
-//         name: "Samsung Galaxy A06",
-//         image:
-//           "https://cdn.tgdd.vn/Products/Images/42/326348/samsung-galaxy-z-fold6-xanh-navy-1-750x500.jpg",
-//         price: "1.490.000 đ",
-//       },
-//       {
-//         id: "baf72de3-9128-453e-8a20-dcecce3f3305",
-//         name: "iPhone 15 Pro Max",
-//         image:
-//           "https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/42/329149/iphone-16-pro-max-titan-den-1-638638962017739954-750x500.jpg",
-//         price: "1.590.000 đ",
-//       },
-//       {
-//         id: "1d489d11-4245-4fc9-b5f3-a37e85c91b6d",
-//         name: "iPhone 16 Plus",
-//         image:
-//           "https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/42/329149/iphone-16-pro-max-titan-den-1-638638962017739954-750x500.jpg",
-//         price: "1.990.000 đ",
-//       },
-//       {
-//         id: "778640d8-1429-4255-aa82-8374d5342a13",
-//         name: "Samsung Galaxy Z Flip 6",
-//         image:
-//           "https://cdn.tgdd.vn/Products/Images/42/326348/samsung-galaxy-z-fold6-xanh-navy-1-750x500.jpg",
-//         price: "1.990.000 đ",
-//       },
-//       {
-//         id: "9bd60040-5503-4dff-95a9-cc5112f49313",
-//         name: "Samsung Galaxy Z Fold 6",
-//         image:
-//           "https://cdn.tgdd.vn/Products/Images/42/326348/samsung-galaxy-z-fold6-xanh-navy-1-750x500.jpg",
-//         price: "1.890.000 đ",
-//       },
-//     ],
-//   },
-// ];
 
 const HomePage = () => {
   const [mockCollections, setAllCollections] = useState({});
   const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
+  const [banners, setBanner] = useState([]);
   const isDesktop = useMediaQuery({ minWidth: 481 });
   useEffect(() => {
     let apiUrl = `${BASE_URL}collection/123e4567-e89b-12d3-a456-426614174000`;
@@ -140,11 +40,61 @@ const HomePage = () => {
         setLoading(false); // Kết thúc loading
       });
   }, []);
+  useEffect(() => {
+    let apiUrl = `${BASE_URL}slideshows?content=banner`;
+    setLoading(true);
+    axiosInstance
+      .get(apiUrl, {
+        headers: {
+          'ngrok-skip-browser-warning': 'true',
+        },
+      })
+      .then((response) => {
+        const dataColletion = response.data.data;
+        setBanner(dataColletion);
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 400) {
+          setBanner([]);
+          setLoading(true);
+        } else {
+          console.error('Error fetching data:', error);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  // const scrollX = useRef(0);
+  // const scrollViewRef = useRef(null);
+  // const { width: windowWidth } = useWindowDimensions();
+  // const intervalRef = useRef(null);
 
+  // useEffect(() => {
+  //   let currentIndex = 0;
+
+  //   intervalRef.current = setInterval(() => {
+  //     if (scrollViewRef.current) {
+  //       currentIndex = (currentIndex + 1) % banners.length;
+  //       scrollX.current = currentIndex * windowWidth;
+
+  //       // Animate scroll using `scrollLeft` for horizontal scrolling
+  //       scrollViewRef.current.scrollTo({
+  //         left: scrollX.current,
+  //         behavior: 'smooth', // Smooth scrolling
+  //       });
+  //     }
+  //   }, 3000); // Auto-scroll every 3 seconds
+
+  //   return () => {
+  //     clearInterval(intervalRef.current); // Clear interval when the component unmounts
+  //   };
+  // }, [windowWidth, banners.length]);
   return (
     <div className="section">
       <div className="container">
         <div className="row">
+          <Banner banners={banners} loading={loading}/>
           <div className="col-md-12">
             <div className="section-title">
               <h3 className="title" style={{ textAlign: 'center' }}>Collections</h3>
